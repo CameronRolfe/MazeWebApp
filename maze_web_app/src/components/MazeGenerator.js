d8c2e230e44df448702633154bb1ee472526be06
Cameron Rolfe <cameronrolfedev@gmail.com>
2024-04-07 19:21:55 -0400

    (Bug fix): Adding clases to tailwind safelist. (Bug fix): Fixed canvas resize on cellSize change.

--- maze_web_app/src/components/MazeGenerator.js
+++ maze_web_app/src/components/MazeGenerator.js
@@ -4,7 +4,8 @@ import MazeButton from "./MazeButton";
 export default function MazeGenerator({maze, cellSize, setMode}) {
     const numRows = maze.gridRows
     const numCols = maze.gridCols
-    const [generating, setGenerating] = useState(true);
+    const [generating, setGenerating] = useState(false);
+    const [canvasSize, setCanvasSize] = useState(null);
     
     const startCell = maze.startGridCell
     const [startRow, startCol] = startCell;
@@ -14,18 +15,18 @@ export default function MazeGenerator({maze, cellSize, setMode}) {
     const delay = 1;
     const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
-    const getCanvasCoords = (cell) => {
+    const getCanvasCoords = (cell, cellSize) => {
         const [row, col] = cell;
         const x = col * cellSize;
         const y = row * cellSize;
         return [x, y]
     }
 
-    const drawCell = async (cell) => {
+    const drawCell = async (cell, cellSize) => {
         const canvas = document.getElementById("mazeCanvas");
         if (!canvas) return;
         const ctx = canvas.getContext("2d");
-        const [x, y] = getCanvasCoords(cell)
+        const [x, y] = getCanvasCoords(cell, cellSize)
         ctx.fillStyle = "white";
         const [row, col] = cell
         if (row === startRow && col === startCol) ctx.fillStyle = "green"
@@ -34,13 +35,16 @@ export default function MazeGenerator({maze, cellSize, setMode}) {
         await sleep(delay)
     }
 
-    const drawMaze = async () => {
-        const generationPath = maze.generationPath
-        for (const cell of generationPath)  await drawCell(cell)
-        setGenerating(false);
-    }
     const canvasHeight = numRows * cellSize;
     const canvasWidth = numCols * cellSize
+    if (canvasSize === null) setCanvasSize([canvasWidth, canvasHeight])
+    const drawMaze = async () => {
+        setGenerating(true)
+        const generationPath = maze.generationPath
+        const currCellSize = cellSize;
+        for (const cell of generationPath)  await drawCell(cell, currCellSize)
+        setGenerating(false);
+    }
 
     useEffect(() => {
         drawMaze();
@@ -49,7 +53,7 @@ export default function MazeGenerator({maze, cellSize, setMode}) {
     return (
         <div className = "w-max mx-auto mt-5 mb-5">
             <MazeButton primaryColour={(generating) ? "red-500" : "blue-500"} buttonText={(generating) ? "Stop Generation" : "Show Maze Image"} onClick={() => setMode("image")}></MazeButton>
-            <canvas className="bg-black mt-5" id="mazeCanvas" width={canvasWidth} height={canvasHeight}></canvas>
+            <canvas className="bg-black mt-5" id="mazeCanvas" width={canvasSize ? canvasSize[0] : null} height={canvasSize ? canvasSize[1] : null}></canvas>
         </div>
     );
 }
--- maze_web_app/tailwind.config.js
+++ maze_web_app/tailwind.config.js
@@ -10,5 +10,15 @@ module.exports = {
     }
   },
   plugins: [],
+  safelist: [
+    "bg-blue-500",
+    "bg-red-500",
+    "text-blue-500",
+    "text-red-500",
+    "border-blue-500",
+    "border-red-500",
+    "hover:bg-blue-500",
+    "hover:bg-red-500"
+  ]
 }
 
